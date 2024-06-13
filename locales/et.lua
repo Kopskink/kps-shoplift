588c83d3341d486734bb982e1c279d5cb21cfeff
Kõpskink <154380199+Kopskink@users.noreply.github.com>
2024-06-13 16:54:33 +0300

    Create et.lua

---
+++ locales/et.lua
@@ -0,0 +1,13 @@
+local Translations = {
+	info = {
+		takingstuff = "Krabad esemeid...",
+        afraid = "Kardad, et müüja võib sind märgata!",
+		citynopolice = "Linn on tühjavõitu",
+	},
+	target = {
+		shoplift = "Kraba esemeid",
+	},
+	dispatch = {
+		message = "Poevargus",
+	},
+}
